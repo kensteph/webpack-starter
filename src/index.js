719b2c29cde99e1b1d9661ba3ff366d1af3369c6
Kender ROMAIN <kenderromain@gmail.com>
2023-01-08 00:09:25 -0400

    Add main.scss file and configure the appropiate loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import generateJokes from './generateJokes';
+import './styles/main.scss';
 
 console.log(generateJokes());
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,20 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  // Entry point for webpack
   entry: {
     bundle: path.resolve(__dirname, 'src/index.js'),
   },
+  // Where we gonna build
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
+  },
+  // Manage the loaders
+  module: {
+    rules: [
+      // Any file ends by this extention,we will use these loaders
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+    ],
   },
 };
